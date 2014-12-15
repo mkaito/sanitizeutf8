1592ff8db95e5aeea1fbb7166c4798386bab5bb7
Michishige Kaito <me@mkaito.com>
2014-12-15 02:00:57 +0000

    Conform to rust style guide and not use tabs.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::io;
 
 fn main() {
-	let mut reader = io::stdin();
-	for c in reader.lock().chars() {
-		match c {
-			Ok(character) => print!("{}", character),
-			Err(_) => continue,
-		}
-	}
+    let mut reader = io::stdin();
+    for c in reader.lock().chars() {
+        match c {
+            Ok(character) => print!("{}", character),
+            Err(_) => continue,
+        }
+    }
 }
